04a0ffffddf1cfc45f37bfafe7f412f975184562
hansa0712004212 <hansa0712004212@gmail.com>
2020-05-16 17:24:23 +0530

    Tale model added

---
+++ lib/model/Tale.dart
@@ -0,0 +1,15 @@
+class Tale {
+  final int id;
+  final String title;
+  final String story;
+  final String image;
+  final List<String> tags;
+
+  Tale({
+    this.id,
+    this.title,
+    this.story,
+    this.image,
+    this.tags
+  });
+}
