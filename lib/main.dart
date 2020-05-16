edf8f948a5f2df4182d1eba16d01fab2c2b7fda3
hansa0712004212 <hansa0712004212@gmail.com>
2020-05-16 22:12:09 +0530

    Custom unicode Sinhala font added
    This helps to display Sinhala text in non Sinhala unicode providing
    devices and OSs.
    Tested on Sony Xperia M2 Aqua, Android Lollipop 5.0

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:jathakakatha/ui/welcome.dart';
 
 void main() {
   runApp(MaterialApp(
+    theme: ThemeData(fontFamily: 'IskoolaPota'),
     home: Welcome(),
     routes: <String, WidgetBuilder>{
       '/Home': (BuildContext context) => Home(),
