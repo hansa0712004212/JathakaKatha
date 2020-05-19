3c9c87ab34b2fd0f0ad63b87a81291be94e25426
hansa0712004212 <hansa0712004212@gmail.com>
2020-05-19 23:42:32 +0530

    focus to default list view when owner peform search

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -117,6 +117,7 @@ class _HomeState extends State<Home> {
                   onChanged: (String searchKey) async {
                     setState(() {
                       _searchKey = searchKey;
+                      _isRecentEnabled = false;
                     });
                   },
                 ),
