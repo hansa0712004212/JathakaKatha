74e3307878124610cc7da020021ff92840a40f0a
hansa0712004212 <hansa0712004212@gmail.com>
2020-05-24 07:47:24 +0530

    AVOID wrapping fields in getters and setters just to be "safe"

--- lib/model/AppPreference.dart
+++ lib/model/AppPreference.dart
@@ -2,48 +2,27 @@ import 'package:jathakakatha/data/sinhala.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AppPreference {
-  bool _isFirstTimeRun;
-  double _storyFontSize;
-  double _ttsSpeed;
-  List<String> _recentIds;
+  bool isFirstTimeRun;
+  double storyFontSize;
+  double ttsSpeed;
+  List<String> recentIds;
   static SharedPreferences sharedPreferences;
 
   AppPreference._();
-
-  bool get isFirstTimeRun =>
-      sharedPreferences.getBool("isFirstTimeRun") ?? true;
-
-  double get storyFontSize => _storyFontSize;
-
-  set storyFontSize(double value) {
-    _storyFontSize = value;
-  }
-
-  double get ttsSpeed => _ttsSpeed;
-
-  set ttsSpeed(double value) {
-    _ttsSpeed = value;
-  }
-
-  List<String> get recentIds => _recentIds;
-
-  set recentIds(List<String> value) {
-    _recentIds = value;
-  }
 
   static Future<AppPreference> getInstance() async {
     sharedPreferences = await SharedPreferences.getInstance();
     if (sharedPreferences.getBool("isFirstTimeRun") ?? true) {
       sharedPreferences.setBool("isFirstTimeRun", false);
       sharedPreferences.setDouble("fontSizeStory", constFontSizeStory);
-      sharedPreferences.setDouble("ttsSpeed", 1.0);
+      sharedPreferences.setDouble("ttsSpeed", 1);
       sharedPreferences.setStringList("recentId", []);
     }
     AppPreference preference = new AppPreference._();
-    preference.storyFontSize =
-        sharedPreferences.getDouble("fontSizeStory") ?? constFontSizeStory;
-    preference.ttsSpeed = sharedPreferences.getDouble("ttsSpeed") ?? 1.0;
-    preference.recentIds = sharedPreferences.getStringList("recentId") ?? [];
+    preference.isFirstTimeRun = sharedPreferences.getBool("isFirstTimeRun");
+    preference.storyFontSize = sharedPreferences.getDouble("fontSizeStory");
+    preference.ttsSpeed = sharedPreferences.getDouble("ttsSpeed");
+    preference.recentIds = sharedPreferences.getStringList("recentId");
     return preference;
   }
 
