99d2bd35ba81f904d8ffd1ed267b25661ffcb9ba
hansa0712004212 <hansa0712004212@gmail.com>
2020-05-31 14:15:57 +0530

    add Configurations load support from JSON as singleton
    https://pub.dev/packages/global_configuration

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -7,6 +7,7 @@ import 'package:jathakakatha/data/sinhala.dart';
 import 'package:jathakakatha/model/AppPreference.dart';
 import 'package:jathakakatha/model/Tale.dart';
 import 'package:jathakakatha/ui/story.dart';
+import 'package:global_configuration/global_configuration.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -25,6 +26,7 @@ class _HomeState extends State<Home> {
   @override
   void initState() {
     super.initState();
+    GlobalConfiguration().loadFromAsset("ad_keys");
     getSharedPreferences();
     _searchKey = "";
     _isRecentEnabled = false;
@@ -73,7 +75,7 @@ class _HomeState extends State<Home> {
                             ? IcoMoonIcons.list2
                             : IcoMoonIcons.history,
                         color: constColorIcon),
-                    showShadow: false,
+                    showShadow: true,
                     shadowColor: constColorIcon),
                 color: constColorIcon,
                 splashColor: constColorIconSplash,
